77effcd081225004cf1eddec1910926ee7241405
fusionlambda <39540795+FusionLambda@users.noreply.github.com>
2020-01-05 14:38:24 -0800

    Core functionality of Chunk
    implemented functions in chunk:
     - isEmpty
     - swap
     - getState
     - setState

--- src/autoChunk.js
+++ src/autoChunk.js
@@ -6,11 +6,38 @@ class Chunk {
     constructor(size) {
         this.size = size;
 
+        //board state, array of cells
         this.currentState = [];
         this.nextState = [];
 
-        this.isEmpty = true;
+        //boolean states for emptiness
+        this.currEmpty;
+        this.nextEmpty;
         
+    }
+    get isEmpty(){
+        return this.currEmpty;
+    }
+    //preps for the next iteration of the automaton
+    //for this chunk
+    swap(){
+        //swap arrays
+        var tmp = this.currentState;
+        this.currentState = this.nextState;
+        this.nextState = tmp;
+
+        //shift empty state
+        this.currEmpty = this.nextEmpty;
+        this.nextEmpty = true;
+    }
+    get getState(x, y){
+        return this.currentState[y * size + x];
+    }
+    setState(x, y, state){
+        this.nextState[y * size + x] = state;
+        if(state != null){
+            this.nextEmpty = false;
+        }
     }
 
 }
--- src/automaton.js
+++ src/automaton.js
@@ -8,11 +8,22 @@ class Automaton {
         this.size = size;
 
         this.loadedChunks = [];
-        this.step
+
+        if(!rule instanceof Function){
+            console.error("Rule is not a function!");
+        }
+        if((!Number.isInteger(size)) || size < 1){
+            console.error("Invalid chunk size: "+size);
+        }
+        
 
         
     }
+    //increments the automaton by the number of steps
     step(steps = 1) {
+        if((!Number.isInteger(steps)) || steps < 1){
+            return;
+        }
 
     }
 
